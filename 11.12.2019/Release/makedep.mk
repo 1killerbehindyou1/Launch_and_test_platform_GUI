345194416d0a050051c9ed5799f88f464d9c39fc
1killerbehindyou1 <46180882+1killerbehindyou1@users.noreply.github.com>
2019-11-12 13:12:57 -0800

    Add files via upload
    wersja z 11.12.2019

---
+++ 11.12.2019/Release/makedep.mk
@@ -0,0 +1,20 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+adc.c
+
+I2C_dev.c
+
+main.c
+
+pot.c
+
+Procedury_dla_main.c
+
+zasilacz.c
+
+PWM.c
+
+uart.c
+
