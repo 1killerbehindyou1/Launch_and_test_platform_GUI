3a4b13d91db9675b395c92ddb77d627c6d610537
1killerbehindyou1 <46180882+1killerbehindyou1@users.noreply.github.com>
2020-01-04 18:18:06 -0800

    Delete prog.bat

--- 11.12.2019/Debug/prog.bat
+++
@@ -1,5 +0,0 @@
-
-cd C:\avr_dude
-avrdude.exe -v -v -p atmega32u4 -c avr109  -P com9   -U flash:w:"C:/Users/Kolunio/Desktop/1.AVR_OPROGRAMOWANIE/1. Platforma_testowa/0. aplikacja na m32U4/platforma/Release/platforma.hex":i
-
-pause
